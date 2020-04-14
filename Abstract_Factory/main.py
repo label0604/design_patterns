ceaacfa7647ed3961aa50967f47d9cdc09abcc90
label0604 <label0604@gmail.com>
2020-04-14 15:23:56 +0900

    Change usage at Abstract Factory

--- Abstract_Factory/main.py
+++ Abstract_Factory/main.py
@@ -5,7 +5,7 @@ from factory import Factory
 if __name__ == '__main__':
     args = sys.argv
     if len(args) != 2:
-        print('Usage: Python main.py factory.ListFactory or facotry.TableFactory')
+        print('Usage: Python main.py ListFactory')
         sys.exit()
 
     factory = Factory.get_factory(args[1])
